b8eac0a5ecfa364730e4f0dcba5296deaaa5728a
Daniel Rech <daniel.prech19@gmail.com>
2026-02-23 10:15:51 -0300

    ajustes na views da playlist 3

--- beats/settings.py
+++ beats/settings.py
@@ -100,19 +100,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'beats.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
-
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
